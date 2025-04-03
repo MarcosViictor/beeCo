e2077d728766e6d51999248cfa61cceb47df08d3
Luis <luiss18.code@gmail.com>
2025-04-02 23:32:52 -0300

    feat: adiciona tipos para Node e configura aliases de caminho no projeto

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,14 @@
+
+export const Footer = () => {
+    return (
+        <footer>
+            <div>
+                <h4>BeeCo</h4>
+                <p>Revolucionando a comunicação entre prestadores de serviço e contratantes.</p>
+                <div>
+
+                </div>
+            </div>
+        </footer>
+    )
+}
