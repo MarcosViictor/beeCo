477ccba8a19635a048323f5f4064eee1ca2f3cac
Luis <luiss18.code@gmail.com>
2025-04-03 00:42:25 -0300

    feat: adiciona nova rota para opção de registro e atualiza estilos e componentes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Teste } from '@/pages/Teste'
-import { Footer } from '@/components/Footer'
-
+import { RegisterOptionUser } from '@/pages/RegisterOptionUser'
 function App() {
   return (
     <BrowserRouter>
       {/* importar header, menu, footer aqui */}
           <Routes>
             <Route path="/" element={<Teste />} />
+            <Route path="/register-option" element={<RegisterOptionUser />} />
           </Routes>
-          <Footer />
+          
       
     </BrowserRouter>
   )
