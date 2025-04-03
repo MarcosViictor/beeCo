3bf35fce304fb9ea5fd9c8e3b29704dfa7c12738
Gustavo Leite <qustavoleite@gmail.com>
2025-04-03 14:56:39 -0300

    feat: header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Teste } from '@/pages/Teste'
 import { Footer } from '@/components/Footer'
+import { Header } from '@/components/Header'
 
 function App() {
   return (
     <BrowserRouter>
       {/* importar header, menu, footer aqui */}
-          <Routes>
-            <Route path="/" element={<Teste />} />
-          </Routes>
-          <Footer />
-      
+      <Header />
+      <Routes>
+        <Route path='/' element={<Teste />} />
+      </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
