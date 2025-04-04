6cef7b0de65e61b015eeac774f5abfad6bb62d0a
Luis <luiss18.code@gmail.com>
2025-04-03 21:24:19 -0300

    feat: substitui a rota inicial para a página Home no componente App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Teste } from '@/pages/Teste'
 import { Login } from '@/pages/Login'
 import { RegisterProfessional } from '@/pages/RegisterProfessional'
 import { RegisterClient } from '@/pages/RegisterClient'
 import { RegisterOptionUser } from '@/pages/RegisterOptionUser'
+import { Home } from '@/pages/Home'
 
 function App() {
   return (
     <BrowserRouter>
       
       <Routes>
-        <Route path='/' element={<Teste />} />
+        <Route path='/' element={<Home />} />
         <Route path="/register-option" element={<RegisterOptionUser />} />
         <Route path="/register-professional" element={<RegisterProfessional />} />
         <Route path="/register-client" element={<RegisterClient />} />
