737fdad66e80f06506fcebd2837b5d562a1aaf51
victor <victorfire99@gmail.com>
2025-05-07 22:33:51 -0300

    fix: ajuste de pastas

--- routes/api.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AvaliacaoController;
-
-Route::apiResource('avaliacoes', AvaliacaoController::class);
